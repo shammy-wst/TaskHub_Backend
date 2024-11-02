d84359b8fa4c4c64d0b3d63d16c71b4a49bc1a06
Icham M'madi <126263598+shammy-wst@users.noreply.github.com>
2024-11-02 05:16:18 +0100

    refactor: conversion en CommonJS et correction du server.js

--- server.js
+++ server.js
@@ -4,13 +4,11 @@ const config = require("./src/config/config");
 
 const server = http.createServer(app);
 
-// Synchronisation avec la base de données
 config.sequelize
   .sync()
   .then(() => {
     server.listen(config.port, () => {
       console.log(`Server running on port ${config.port}`);
-      console.log("Database synchronized and Task model is ready");
     });
   })
   .catch((error) => {
